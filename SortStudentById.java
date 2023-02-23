470515bfb29da450fa99a24e0be7bf80f8a71575
Shashidhar <126139138+ShashidharV26@users.noreply.github.com>
2023-02-23 22:09:01 +0530

    Add files via upload

---
+++ SortStudentById.java
@@ -0,0 +1,13 @@
+package costomSorting;
+
+import java.util.Comparator;
+
+import sdbms.Student;
+
+public class SortStudentById implements Comparator<Student> {
+	@Override
+	public int compare(Student x, Student y) {
+		return x.getId().compareTo(y.getId());
+	}
+}
+// x-->object to be inserted    y-->already existing object
